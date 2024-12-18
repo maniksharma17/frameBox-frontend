3426b1369a744b763e17fa5e2a8822ae5a750819
Manik Sharma <maniksharma.rke@gmail.com>
2024-12-18 22:05:12 +0530

    base url modified

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: "/frameBox-frontend",
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
